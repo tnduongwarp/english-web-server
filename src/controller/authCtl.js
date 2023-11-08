5a36109c4b68fee60d50a1493d5248c8a16ed7c7
duongnt204538 <duong.nt204538@sis.hust.edu.vn>
2023-11-08 21:51:52 +0700

    modify .gitignore and modify reponse

--- src/controller/authCtl.js
+++ src/controller/authCtl.js
@@ -9,52 +9,56 @@ import {
 import { refreshTokenBodyValidation } from '../utils/validationSchema.js';
 import { Sequelize } from 'sequelize';
 const authCtl = {
-    login: async (req,res) => {
+    login: async (req, res) => {
         try {
-          const { error } = logInBodyValidation(req.body);
-          if (error)
-              return res
-                  .status(400)
-                  .json({ error: true, message: error.details[0].message });
-            var user = await db.User.findOne({ 
+            const { error } = logInBodyValidation(req.body);
+            if (error)
+                return res
+                    .status(400)
+                    .json({ error: true, message: error.details[0].message });
+            var user = await db.User.findOne({
                 where:
-                    {
-                        username: req.body.username 
-                    }
+                {
+                    username: req.body.username
+                }
             });
-            if (user && (await bcrypt.compare(req.body.password, user.password)) ) 
-            {
-              const { accessToken, refreshToken } = await generateTokens(user);
-             
-              res.status(200).json({
-                error: false,
-                message: 'Login successfully!',
-                user,
-                accessToken,
-                refreshToken
-              });
+            if (user && (await bcrypt.compare(req.body.password, user.password))) {
+                const { accessToken, refreshToken } = await generateTokens(user);
+
+                res.status(200).json({
+                    error: false,
+                    message: 'Login successfully!',
+                    user,
+                    accessToken,
+                    refreshToken
+                });
             }
-            else res.status(400).send("Invalid Credentials");
-          } catch (err) {
-                console.log(err);
-                res.status(500).json({ error: true, message: "Internal Server Error" });
-            }
+            else res.status(401).json({
+                error: true,
+                message: 'Invalid creditials!'
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
+        }
     },
 
-     signUp: async (req, res) => {
+    signUp: async (req, res) => {
         try {
             const { error } = signUpBodyValidation(req.body);
             if (error)
                 return res
                     .status(400)
                     .json({ error: true, message: error.details[0].message });
-    
+
             const user = await db.User.findOne({
-                                              where:{  
-                                                [Sequelize.Op.or]: [
-                                                  { username: req.body.username },
-                                                  { email: req.body.email }
-                                                ] }});
+                where: {
+                    [Sequelize.Op.or]: [
+                        { username: req.body.username },
+                        { email: req.body.email }
+                    ]
+                }
+            });
             if (user?.username === req.body.username)
                 return res
                     .status(400)
@@ -63,19 +67,19 @@ const authCtl = {
                 return res
                     .status(400)
                     .json({ error: true, message: "User with given email already exist" });
-    
+
             const salt = await bcrypt.genSalt(Number(process.env.SALT));
             const hashPassword = await bcrypt.hash(req.body.password, salt);
-    
+
             await db.User.create({
-              username: req.body.username,
-              password: bcrypt.hashSync(req.body?.password,salt),
-              email: req.body.email,
-              registration_date: new Date(),
-              profile_picture: req.body?.profile_picture,
-              role: req.body.role
-          })
-    
+                username: req.body.username,
+                password: bcrypt.hashSync(req.body?.password, salt),
+                email: req.body.email,
+                registration_date: new Date(),
+                profile_picture: req.body?.profile_picture,
+                role: req.body.role
+            })
+
             res
                 .status(201)
                 .json({ error: false, message: "Account created sucessfully" });
@@ -83,32 +87,32 @@ const authCtl = {
             console.log(err);
             res.status(500).json({ error: true, message: "Internal Server Error" });
         }
-      },
+    },
 
-      logOut: async (req,res) => {
+    logOut: async (req, res) => {
         try {
-          const { error } = refreshTokenBodyValidation(req.body);
-          if (error)
-              return res
-                  .status(400)
-                  .json({ error: true, message: error.details[0].message });
-  
-          const userToken = await db.user_token.findOne({ where: {token: req.body.refreshToken} });
-          if (!userToken)
-              return res
-                  .status(200)
-                  .json({ error: false, message: "Logged Out Sucessfully" });
-  
-          await db.user_token.destroy({
-                where:{
+            const { error } = refreshTokenBodyValidation(req.body);
+            if (error)
+                return res
+                    .status(400)
+                    .json({ error: true, message: error.details[0].message });
+
+            const userToken = await db.user_token.findOne({ where: { token: req.body.refreshToken } });
+            if (!userToken)
+                return res
+                    .status(200)
+                    .json({ error: false, message: "Logged Out Sucessfully" });
+
+            await db.user_token.destroy({
+                where: {
                     userId: userToken.id
                 }
             });
-          res.status(200).json({ error: false, message: "Logged Out Sucessfully" });
-      } catch (err) {
-          console.log(err);
-          res.status(500).json({ error: true, message: "Internal Server Error" });
-      }
-      }
+            res.status(200).json({ error: false, message: "Logged Out Sucessfully" });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: true, message: "Internal Server Error" });
+        }
+    }
 }
 export default authCtl;
